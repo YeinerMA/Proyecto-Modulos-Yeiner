d0aeaf6f2c575850879d1eb3a4ad1d5ecae51759
YeinerMA <ymezaa@ctpplatanares.ed.cr>
2025-08-22 16:05:13 -0600

    Yeiner meza

--- analisis_datos/estadisticas.py
+++ analisis_datos/estadisticas.py
@@ -1,1 +1,20 @@
 # estadisticas.py
+def media(datos):
+    media_aritmetica = sum(datos) / len(datos)
+    return media_aritmetica
+
+def mediana(datos):
+    
+    
+    datos = sorted(datos)
+    n = len(datos)
+    mitad = n // 2
+    if n % 2 == 0:
+        mediana ((datos[mitad -1]+ datos[mitad])/2)
+    else:
+        mediana = datos[mitad]
+    return mediana
+
+if __name__=='__main__':
+    notas = (100,50,100,78,100)
+    print(media(notas))
