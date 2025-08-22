954b890858be7074694dab1bbf0bcddf90bd1dd2
YeinerMA <ymezaa@ctpplatanares.ed.cr>
2025-08-22 13:25:13 -0600

    yo

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Class05_Archivos_Modularizacion_Python
  Gestión de Archivos y Modularización en Python
  Prof. Andrés Mena Abarca
+#Autor: Yeiner Meza Aguilar
+Fecha 22/08/2025
--- main.py
+++ main.py
@@ -5,3 +5,9 @@
 # Descripción: Este archivo contiene el punto de entrada principal del proyecto.
 # Recuerda incluir tu nombre completo, la fecha en la que iniciaste el proyecto y la fecha estimada de entrega.
 # Esto ayuda a mantener un registro claro del trabajo realizado.
+
+
+def saludar():
+    print('hola desde la funcion')
+    if __name__ == '__main__':
+        saludar()
